8fe24a83f62667cfb55b05fe69609b14098be805
toji <tojiro@gmail.com>
2021-05-11 00:05:36 +0000

    toji published a site update

--- js/webgpu-renderer/webgpu-renderer.js
+++ js/webgpu-renderer/webgpu-renderer.js
@@ -99,6 +99,7 @@ export class WebGPURenderer extends Renderer {
       // attachment is acquired and set in onFrame.
       resolveTarget: undefined,
       loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+      storeOp: 'clear',
     };
 
     this.depthAttachment = {
