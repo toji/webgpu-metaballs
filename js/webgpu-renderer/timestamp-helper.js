7ed5434466f72e2f27f63aeb61cc454dbdd6725c
Brandon Jones <tojiro@gmail.com>
2026-05-13 15:05:05 -0700

    Refactor timestamp helper

--- js/webgpu-renderer/timestamp-helper.js
+++ js/webgpu-renderer/timestamp-helper.js
@@ -1,60 +1,48 @@
-/** Number of timing samples to collect before computing the average */
-const AVG_SAMPLE_COUNT = 30;
-
-/**
- * A utility that makes it a bit easier to gather timings from WebGPU compute and render passes.
- */
-export class TimestampHelper {
-  device;
-  #timestampsSupported = false;
-  #timestampQuerySet;
-  #timestampResolveBuffer;
-  #timestampReadbackBuffers = [];
-  #readbackBufferCount = 0;
-  #currentReadbackBuffer = null;
-  #passTimings = new Map();
-  #maxPassCount = 0;
+class TimestampQueryData {
+  #recycleCallback = null;
+  #maxQueryCount = 0;
   #nextQueryIndex = 0;
+  #querySet;
+  #resolveBuffer;
+  #readbackBuffer;
   #queriesUsed = new Map();
-  #averages = {};
 
-  constructor(device, maxPassCount = 16) {
-    this.device = device;
-    this.#maxPassCount = maxPassCount;
-    this.#timestampsSupported = this.device.features.has("timestamp-query");
-    if (this.#timestampsSupported) {
-      this.#timestampQuerySet = this.device.createQuerySet({
-        label: "Timestamp Helper",
-        type: "timestamp",
-        count: this.#maxPassCount,
-      });
-      this.#timestampResolveBuffer = this.device.createBuffer({
-        size: BigUint64Array.BYTES_PER_ELEMENT * this.#maxPassCount,
-        usage: GPUBufferUsage.QUERY_RESOLVE | GPUBufferUsage.COPY_SRC,
-      });
-    }
+  constructor(device, maxQueryCount, recycleCallback) {
+    this.#maxQueryCount = maxQueryCount;
+    this.#recycleCallback = recycleCallback;
+
+    this.#querySet = device.createQuerySet({
+      label: 'Timestamp Helper',
+      type: 'timestamp',
+      count: maxQueryCount,
+    });
+    this.#resolveBuffer = device.createBuffer({
+      label: 'Timestamp Resolve Buffer',
+      size: BigUint64Array.BYTES_PER_ELEMENT * maxQueryCount,
+      usage: GPUBufferUsage.QUERY_RESOLVE | GPUBufferUsage.COPY_SRC,
+    });
+   this.#readbackBuffer = device.createBuffer({
+      label: 'Timestamp Readback Buffer',
+      size: this.#resolveBuffer.size,
+      usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+    });
   }
 
-  get timestampsSupported() {
-    return this.#timestampsSupported;
+  hasQueries() {
+    return this.#nextQueryIndex > 0;
   }
 
-  timestampWrites(name) {
-    if (!this.#timestampsSupported) {
+  getTimestampWrites(name) {
+    if (this.#nextQueryIndex >= this.#maxQueryCount) {
+      console.Error("Exceeded the number of passes that can be queried in a single resolve.");
       return undefined;
     }
-    if (this.#currentReadbackBuffer) {
-      throw new Error(
-        "Must read back the previous resolve before new timestampes can be added.",
-      );
-    }
-    if (this.#nextQueryIndex >= this.#maxPassCount * 2) {
-      throw new Error(
-        "Exceeded the number of passes that can be queried in a single resolve.",
-      );
+    if (this.#queriesUsed.has(name)) {
+      console.Error("Cannot use the came query name twice in a single resolve.");
+      return undefined;
     }
     const timestampWrites = {
-      querySet: this.#timestampQuerySet,
+      querySet: this.#querySet,
       beginningOfPassWriteIndex: this.#nextQueryIndex,
       endOfPassWriteIndex: this.#nextQueryIndex + 1,
     };
@@ -67,108 +55,117 @@ export class TimestampHelper {
   }
 
   resolve(commandEncoder) {
-    if (!this.#timestampsSupported) {
-      return;
-    }
-    if (this.#currentReadbackBuffer) {
-      throw new Error(
-        "Must read back the previous resolve before resolve can be called again.",
-      );
-    }
-    if (this.#timestampReadbackBuffers.length > 0) {
-      this.#currentReadbackBuffer = this.#timestampReadbackBuffers.pop();
-    } else {
-      this.#currentReadbackBuffer = this.device.createBuffer({
-        label: `Timestamp Readback ${this.#readbackBufferCount}`,
-        size: this.#timestampResolveBuffer.size,
-        usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
-      });
-      this.#readbackBufferCount++;
-    }
     commandEncoder.resolveQuerySet(
-      this.#timestampQuerySet,
+      this.#querySet,
       0,
       this.#nextQueryIndex,
-      this.#timestampResolveBuffer,
+      this.#resolveBuffer,
       0,
     );
     commandEncoder.copyBufferToBuffer(
-      this.#timestampResolveBuffer,
+      this.#resolveBuffer,
       0,
-      this.#currentReadbackBuffer,
+      this.#readbackBuffer,
       0,
-      this.#timestampResolveBuffer.size,
+      this.#resolveBuffer.size,
     );
   }
 
   async read() {
-    if (!this.#currentReadbackBuffer) {
-      return;
+    const results = {};
+
+    await this.#readbackBuffer.mapAsync(GPUMapMode.READ);
+    const mappedArray = new BigUint64Array(this.#readbackBuffer.getMappedRange());
+
+    for (const [name, query] of this.#queriesUsed.entries()) {
+      const passTime = Number(mappedArray[query.end] - mappedArray[query.begin]);
+      if (passTime >= 0) {
+        // Return pass timings in µs
+        results[name] = passTime / 1000;
+      } else {
+        // Discard negative times
+        results[name] = 0;
+      }
     }
-    let readbackBuffer = this.#currentReadbackBuffer;
-    let queries = new Map(this.#queriesUsed);
-    this.#currentReadbackBuffer = null;
+
+    this.#readbackBuffer.unmap();
+
+    // Now that the readback is complete, recycle the data for use in a future frame.
     this.#queriesUsed.clear();
     this.#nextQueryIndex = 0;
-    await readbackBuffer.mapAsync(GPUMapMode.READ);
-    const mappedArray = new BigUint64Array(readbackBuffer.getMappedRange());
-    const results = {};
-    for (const [name, query] of queries.entries()) {
-      const passTime = Number(
-        mappedArray[query.end] - mappedArray[query.begin],
-      );
-      // Discard negative times
-      if (passTime >= 0) {
-        let passTimings = this.#passTimings.get(name);
-        if (!passTimings) {
-          passTimings = {
-            values: new Array(AVG_SAMPLE_COUNT),
-            index: 0,
-          };
-          this.#passTimings.set(name, passTimings);
-        }
-        // Storing pass timings in µs
-        let passTimeMicro = passTime / 1000;
-        passTimings.values[passTimings.index++ % AVG_SAMPLE_COUNT] =
-          passTimeMicro;
-        if (passTimings.index % AVG_SAMPLE_COUNT == 0) {
-          // Update the average
-          let avg = 0;
-          for (const value of passTimings.values) {
-            avg += value;
-          }
-          this.#averages[name] = avg / AVG_SAMPLE_COUNT;
-        }
-        results[name] = passTimeMicro;
-      }
-    }
-    // Update any existing keys not in the queries to 0. This way if one of them
-    // isn't present in the readback it will zero out instead of appearing to
-    // stay steady at the last reading.
-    for (const name of this.#passTimings.keys()) {
-      if (!queries.has(name)) {
-        let passTimings = this.#passTimings.get(name);
-        passTimings.values[passTimings.index++ % AVG_SAMPLE_COUNT] = 0;
-        if (passTimings.index % AVG_SAMPLE_COUNT == 0) {
-          // Update the average
-          let avg = 0;
-          for (const value of passTimings.values) {
-            avg += value;
-          }
-          this.#averages[name] = avg / AVG_SAMPLE_COUNT;
-        }
-      }
-    }
-    readbackBuffer.unmap();
-    this.#timestampReadbackBuffers.push(readbackBuffer);
+    this.#recycleCallback(this);
+
     return results;
   }
+}
 
-  hasPendingResults() {
-    return this.#nextQueryIndex != 0;
+class TimestampResults {
+  #queryData;
+  #resultPromise;
+
+  constructor(queryData) {
+    this.#queryData = queryData;
   }
 
-  get averages() {
-    return this.#averages;
+  read() {
+    if (!this.#resultPromise) {
+      this.#resultPromise = this.#queryData?.read() ?? Promise.Resolve({});
+      this.#queryData = null;
+    }
+    return this.#resultPromise
+  }
+}
+
+/**
+ * A utility that makes it a bit easier to gather timings from WebGPU compute and render passes.
+ */
+export class TimestampHelper {
+  device;
+  #timestampsSupported = false;
+
+  #readyQueryData = [];
+  #currentQueryData = null;
+
+  #maxQueryCount = 0;
+
+  constructor(device, maxPassCount = 16) {
+    this.device = device;
+    this.#maxQueryCount = maxPassCount * 2;
+    this.#timestampsSupported = this.device.features.has("timestamp-query");
+    if (this.#timestampsSupported) {
+      this.#currentQueryData = this.#getOrCreateQueryData();
+    }
+  }
+
+  #getOrCreateQueryData() {
+    if (this.#readyQueryData.length) {
+      return this.#readyQueryData.pop();
+    }
+    return new TimestampQueryData(this.device, this.#maxQueryCount, (queryData) => {
+      // Recycle the query data buffers once they've been read back.
+      this.#readyQueryData.push(queryData);
+    });
+  }
+
+  get timestampsSupported() {
+    return this.#timestampsSupported;
+  }
+
+  timestampWrites(name) {
+    if (!this.#timestampsSupported) {
+      return undefined;
+    }
+    return this.#currentQueryData.getTimestampWrites(name);
+  }
+
+  resolve(commandEncoder) {
+    if (!this.#timestampsSupported || !this.#currentQueryData?.hasQueries()) {
+      return new TimestampResults(null);
+    }
+
+    const queryData = this.#currentQueryData;
+    this.#currentQueryData = this.#getOrCreateQueryData();
+    queryData.resolve(commandEncoder);
+    return new TimestampResults(queryData);
   }
 }
